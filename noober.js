cf9c71d13d18d486b12f574165585a7c6880b1fb
Taylor Cathcart <taylor.cathcart@kellogg.northwestern.edu>
2021-06-14 10:51:53 -0500

    COMPLETE

--- noober.js
+++ noober.js
@@ -11,7 +11,36 @@ document.addEventListener('DOMContentLoaded', async function() {
   console.log(ride)
 
   // 🔥 YOUR CODE GOES HERE 🔥
-  // Write the recipe (algorithm) in the comments. Then, write the code.
+  // assign values from ride json to local variables
+  let pickup = ride.pickupLocation.address + ", " + ride.pickupLocation.city + ", " +  ride.pickupLocation.state + " " + ride.pickupLocation.zip
+  let dropoff = ride.dropoffLocation.address + ", " + ride.dropoffLocation.city + ", " +  ride.dropoffLocation.state + " " + ride.dropoffLocation.zip
+  let passNum = ride.numberOfPassengers
+  let passName = ride.passengerDetails.first + " " + ride.passengerDetails.last
+  let phoneNumber = ride.passengerDetails.phoneNumber
+  let purple = ride.purpleRequested
+
+  // build string 
+  let string = "Passenger: " +
+    passName + " - " + phoneNumber + ". " +
+    "Pickup at " + pickup + ". " +
+    "Drop-off at " + dropoff + ". " +
+    passNum + " passengers."
+
+  // check level of service
+  // if noober purple then
+  if (purple) {
+    // prepend noober purple
+    string = "Noober Purple " + string
+  }
+  // elif > 3 passengers then
+  else if (passNum > 3) {
+    //prepend noober xl 
+    string = "Noober XL " + string
+  }
+  else {
+    string = "Noober X " + string
+  }
+  console.log(string)
   
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
